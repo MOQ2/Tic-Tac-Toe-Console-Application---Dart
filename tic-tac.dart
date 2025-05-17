867666f3b0d4050b21eb60a4190917280a48caff
Mohammad Qady <112632549+MOQ2@users.noreply.github.com>
2025-05-17 04:50:55 +0300

    /**  * COMMIT 2: Player model implementation  * - Created PlayerMove enum  * - Player class implementation with basic functionality  */

--- tic-tac.dart
+++ tic-tac.dart
@@ -50,3 +50,64 @@ void main() {
     }
   }
 }
+
+
+
+// Enum representing player moves with associated values
+enum PlayerMove {
+  player1(1),   // Player 1 represented by 1
+  player2(-1);  // Player 2 represented by -1
+
+  final int value;
+  const PlayerMove(this.value);
+}
+
+/// Player class that manages player information and interactions
+class Player {
+  String? name;      // Player's name
+  String? choice;    // Player's symbol (X or O)
+  int winingsCount = 0;  // Number of wins
+  int position = -1;     // Selected position on board
+  
+  // Constructor with default values
+  Player([this.name = "player", this.winingsCount = 0, this.choice = 'X']);
+
+  // Display player information
+  void printInfo() {
+    print("name: $name\nnumber of winings: $winingsCount\nsymbol choice: $choice\n");
+  }
+
+  // Return player information as a map
+  Map<String, dynamic> getInfo() {
+    return {
+      "name": name,
+      "winingsCount": winingsCount,
+      "choice": choice
+    };
+  }
+
+  // Get player's move from console input
+  int getMove() {
+    String? input = null;
+    int position = -1;
+    
+    // Loop until valid position is entered
+    while (position == -1) {
+      print("$name: Please Enter your move (number from 1-9, make sure it is not selected previously):\n");
+      input = stdin.readLineSync();
+      
+      if (input != null) {
+        try {
+          position = int.parse(input);
+        } catch (e) {
+          print("Make sure to enter number from 1-9!!! :\n$e"); 
+        }
+      } else {
+        print("Enter a number from 1-9!");
+      }
+    }
+    
+    this.position = position; 
+    return position;
+  }
+}
